196eef800d1b86b22f7cbbb084ac1a45239c96e4
walt24 <wandujar24@gmail.com>
2017-08-13 16:27:39 -0500

    Update fizz-buzzer-test.js

--- test/fizz-buzzer-test.js
+++ test/fizz-buzzer-test.js
@@ -2,3 +2,17 @@ const fizzBuzzer = require('../fizzBuzzer');
 const should = require('chai').should();
 
 
+describe('FizzBuzzer', function(){
+	it('FizzBuzzer: multiples of 3 must return fizz',function(){
+		let ans = fizzBuzzer(6);
+		ans.should.equal('fizz')
+	});
+	it('FizzBuzzer: multiples of 5 must return buzz',function(){
+		let ans = fizzBuzzer(5);
+		ans.should.equal('buzz')
+	});
+	it('FizzBuzzer: multiples of 15 must return fizz-buzz',function(){
+		let ans = fizzBuzzer(15);
+		ans.should.equal('fizz-buzz')
+	})
+})
